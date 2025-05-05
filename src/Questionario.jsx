60eda062bdd14b6ef575fd6b1e3c57c3b2cb2dbf
=Henrique <=souzahenriquerodrigues25@gmail.com>
2025-05-05 16:06:03 -0300

    adicionado funcionalidade de verificar se questao esta correta/errada

--- src/Questionario.jsx
+++ src/Questionario.jsx
@@ -2,6 +2,14 @@ import Questao from "./Questao";
 
 export default function Questionario({ listaPerguntas }) {
   function handleResposta(respostaUsuario, idQuestao) {
+    for (let i = 0; i <= listaPerguntas.length; i++) {
+      if (listaPerguntas[i].id == idQuestao && respostaUsuario == listaPerguntas[i].resposta) {
+        console.log("Acertou questao");
+      } else if (listaPerguntas[i].id == idQuestao && respostaUsuario != listaPerguntas[i].resposta) {
+        console.log("Errou questao");
+      }
+    }
+
     if (respostaUsuario == true) {
       console.log(`Marcou ${respostaUsuario} na questao de ID : ${idQuestao}`);
     } else {
