276afde754595419a9e3dc111ec522a863097165
=Henrique <=souzahenriquerodrigues25@gmail.com>
2025-05-05 18:58:06 -0300

    adicionado verificador parcial de qtd respostas corretas

--- src/Questionario.jsx
+++ src/Questionario.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Questao from "./Questao";
 
 export default function Questionario({ listaPerguntas }) {
+  const [respostasCorretas, setRespostasCorretas] = useState(0);
+
   function handleResposta(respostaUsuario, idQuestao) {
     listaPerguntas.forEach((objetoPergunta) => {
       if (objetoPergunta.id == idQuestao && respostaUsuario == objetoPergunta.resposta) {
         console.log("Acertou questao");
+        setRespostasCorretas(respostasCorretas + 1);
       } else {
         console.log("Errou questao");
       }
@@ -13,6 +17,7 @@ export default function Questionario({ listaPerguntas }) {
 
   return (
     <div>
+      <h2>Quantidade respostas corretas = {respostasCorretas}</h2>
       <h1>Componente pai da questao</h1>
       <div className="mainWrapper">
         {listaPerguntas.map((objetoPergunta) => {
